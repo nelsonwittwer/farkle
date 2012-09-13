104264525c01f5b95ffd207e6d26cbfd453e80a5
Nelson <nelsonwittwer@gmail.com>
2012-09-12 21:03:19 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Farkle::Application.config.secret_token = 'cdb0561241ca42bca4cea83ca3edc09efae5881dd306f9363f3eb72f73bd0a7664a01ce95e84ed9152fd395662903e5e71cdbdbe44d127e56d72e3e9b86e5e2e'
